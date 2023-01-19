a290a7a4d22d48f1794271ecd39f92eb6c62646c
Chani <111758012+Chansis@users.noreply.github.com>
2023-01-19 11:27:20 +0900

    Data rotiation matlab code

---
+++ Data/MNIST_Rotate/regression_example_matlab.m
@@ -0,0 +1,56 @@
+[XTrain,~,YTrain] = digitTrain4DArrayData;
+[XValidation,~,YValidation] = digitTest4DArrayData;
+
+numTrainImages = numel(YTrain);
+figure
+idx = randperm(numTrainImages,20);
+for i = 1:numel(idx)
+    subplot(4,5,i)    
+    imshow(XTrain(:,:,:,idx(i)))
+end
+
+figure
+histogram(YTrain)
+axis tight
+ylabel('Counts')
+xlabel('Rotation Angle')
+
+layers = [
+    imageInputLayer([28 28 1])
+    convolution2dLayer(3,8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    averagePooling2dLayer(2,'Stride',2)
+    convolution2dLayer(3,16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    averagePooling2dLayer(2,'Stride',2)
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    dropoutLayer(0.2)
+    fullyConnectedLayer(1)
+    regressionLayer];
+
+
+miniBatchSize  = 128;
+validationFrequency = floor(numel(YTrain)/miniBatchSize);
+options = trainingOptions('sgdm', ...
+    'MiniBatchSize',miniBatchSize, ...
+    'MaxEpochs',30, ...
+    'InitialLearnRate',1e-3, ...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropFactor',0.1, ...
+    'LearnRateDropPeriod',20, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',{XValidation,YValidation}, ...
+    'ValidationFrequency',validationFrequency, ...
+    'Plots','training-progress', ...
+    'Verbose',false)
+
+net = trainNetwork(XTrain,YTrain,layers,options);
+
+net.Layers
